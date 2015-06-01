77685f3400f147e730cd55f2c279c2d568498dcc
s4weng <steve_weng@hotmail.ca>
2015-06-01 14:42:39 -0400

    Added daytime server implementation.

--- GetHeadInfo.go
+++ GetHeadInfo.go
@@ -39,7 +39,7 @@ func checkError(err error) {
 
 	if err != nil {
 
-			fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+			fmt.Fprintf(os.Stderr, "Fatal error: %s ", err.Error())
 			os.Exit(1)
 	}
 }
